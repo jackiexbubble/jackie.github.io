e639aaee8022a7e762fa1388e45061d1658629cf
Jackie Lua <Jackie@Jiarens-MacBook-Pro.local>
2021-01-26 16:40:54 +0800

    First commits

---
+++ scripts.js
@@ -0,0 +1,17 @@
+function displayMenu() 
+{
+  var x = document.getElementById("side-nav");
+    if (x.style.display === "block") 
+    {
+      x.style.display = "none";
+    } 
+    else 
+    {
+      x.style.display = "block";
+    }
+}
+
+function showNavMenu() 
+{
+  var x = document.getElementById("side-nav");
+}
